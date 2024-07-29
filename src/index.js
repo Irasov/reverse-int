a1444dda21da5635448003920d77cc097713a68e
Irasov <srnan@yandex.ru>
2024-07-29 15:16:10 +0500

    init: start Reverse Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  s = n.toString();
+  ss = "";
+  for (let i = s.length; i > 0; i--) {
+    if (s[i-1] != '-'){
+        ss += s[i-1];
+    }
+  }
+  return Number(ss);
 }
